4ab36b2af5e5ec70b9d9310b0c9b2a6cede6bb3c
Bison Hubert <bisonhubert@gmail.com>
2022-11-29 22:53:05 -0800

    Finish utests for league

--- tests/test_league.py
+++ tests/test_league.py
@@ -1,12 +1,17 @@
 import pytest
 
 from matchday.constants import MATCH_RECORDS, MATCHDAY_RECORDS
-from matchday.dataclasses import League
+from matchday.dataclasses import League, Matchday
 
 
 def test_initialize_league():
     league = League()
+    matchday = Matchday(1)
     assert league.matchdays is None
     assert repr(league) == "League(matchdays=None, team_count=None)"
     assert league.prev_matchday is None
-    # todobison league.add_matchday()
+    assert league.add_matchday(matchday) is None
+    assert len(league.matchdays) == 1
+    assert matchday in league.matchdays
+    assert league.get_team('team does not exist') is None
+    assert len(league.get_report()) == 1
